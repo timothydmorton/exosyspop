657d7bbfdc8efe8cfe3adb665c0549d0ecb2f30d
Timothy Morton <tim.morton@gmail.com>
2016-04-22 11:25:38 -0400

    updated abc file

--- exosyspop/abc.py
+++ exosyspop/abc.py
@@ -1,37 +1,43 @@
 from __future__ import division, print_function
 
+from simpleabc.simple_abc import Model, basic_abc, pmc_abc
+
 import numpy as np
-from scipy.stats import gaussian_kde, entropy
+from scipy.stats import gaussian_kde, entropy, uniform
 import logging
 
-from .populations import BinaryPopulation
+class ABCModel(Model):
 
-class ABCModel(object):
+    params = ('fB', 'beta', 'beta_a', 'beta_b') # Names of parameters
+    summary_stat_names = ('period_pdf','N',
+                          'phase_sec') # names of summary statistics
+    distance_functions = ('d_period', 'd_N',
+                          'd_fsec', 'd_phase') # names of different distance function methods 
 
-    params = ('fB', 'beta') # Names of parameters
-    summary_stat_names = ('period_pdf','N') # names of summary statistics
-    distance_functions = ('d_period', 'd_N') # names of different distance function methods 
-                              #  length same as summary_stat_names
-    theta_0 = (0.14, -0.95)
-    bounds = ((0,1), (-1.5,0))
-    priors = ('uniform', 'uniform')
-
+    theta_0 = (0.14, -0.95, 0.8, 2.0)
+    bounds = [(0,1), (-1.5,0), (0,5),(0,5)]
+    prior = [uniform(0,1), uniform(-1.5, 1.5), uniform(0,5), uniform(0,5)]    
+    
+    
     def __init__(self, population, eff=None):
         self.population = population
         self.eff = eff
 
     def draw_theta(self):
-        theta = []
-        for p,(lo,hi) in zip(self.priors, self.bounds):
-            if p=='uniform':
-                theta.append(np.random.random()*(hi-lo) + lo)
-        return theta
+        return [p.rvs() for p in self.prior]
+        #theta = []
+        #for p,(lo,hi) in zip(self.priors, self.bounds):
+        #    if p=='uniform':
+        #        theta.append(np.random.random()*(hi-lo) + lo)
+        #return theta
 
     def generate_data(self, theta):
         param_dict = {p:v for p,v in zip(self.params, theta)}
         self.population.set_params(**param_dict)
         try:
             return self.population.observe(new=True, regr_trap=True).observe(self.eff)
+        except KeyboardInterrupt:
+            raise
         except:
             logging.warning('Error generating data: {}'.format(param_dict))
 
@@ -47,18 +53,20 @@ class ABCModel(object):
         """Returns tuple containing summary statistics named in summary_stat_names
         """
         if data is None:
-            return [None]*len(summary_stat_names)
+            return [None]*len(self.summary_stat_names)
 
         N = len(data)
         min_logP, max_logP = np.log(self.min_period), np.log(self.max_period)
         logP_grid = np.linspace(min_logP, max_logP, 1000)
         if N > 1:
             k = gaussian_kde(np.log(data.period.values))
-            pdf = k(logP_grid)
+            logP_pdf = k(logP_grid)
         else:
-            pdf = np.ones(len(logP_grid))*1./(max_logP - min_logP)
+            logP_pdf = np.ones(len(logP_grid))*1./(max_logP - min_logP)
 
-        return pdf, N
+        phase_sec = data.phase_sec.dropna().values
+            
+        return logP_pdf, N, phase_sec
 
     def d_period(self, summary_stats, summary_stats_synth):
         p1 = summary_stats[0]
@@ -72,7 +80,7 @@ class ABCModel(object):
         return kl_period
     
     def Ndist(self, N1, N2):
-        if N1==0. or N2==0.:
+        if N1==0. or N2==0. or N1 is None or N2 is None:
             dist = 1
         else:
             dist = max(1 - 1.*N1/N2, 1-1*N2/N1)
@@ -81,9 +89,40 @@ class ABCModel(object):
     def d_N(self, summary_stats, summary_stats_synth):
         N1 = summary_stats[1]
         N2 = summary_stats_synth[1]
-        return self.Ndist(N1, N2) 
+        d = self.Ndist(N1, N2)
+        #logging.info('{}, {}, {}'.format(N1, N2, d))
+        return d 
         
+    def d_fsec(self, summary_stats, summary_stats_synth):
+        N1, phase_sec1 = summary_stats[1:3]
+        N2, phase_sec2 = summary_stats_synth[1:3]
+        
+        f_sec1 = len(phase_sec1)/float(N1)
+        f_sec2 = len(phase_sec2)/float(N2)
+        
+        return np.absolute(f_sec1 - f_sec2)
 
+    def d_phase(self, summary_stats, summary_stats_synth, nbins=11):
+        phase_sec1 = summary_stats[2]
+        phase_sec2 = summary_stats_synth[2]
+
+        try:
+            len(phase_sec2)
+        except:
+            return np.inf
+        
+        if len(phase_sec1) < 2 or len(phase_sec2) < 2:
+            return np.inf
+        
+        k1 = gaussian_kde(phase_sec1)
+        k2 = gaussian_kde(phase_sec2)
+
+        phs = np.linspace(0,1,100)
+        pdf1 = k1(phs)
+        pdf2 = k2(phs)
+        
+        return entropy(pdf1, pdf2)
+        
     def null_distance_test(self, theta=None, N=100):
         if theta is None:
             theta = self.theta_0
@@ -100,7 +139,10 @@ class ABCModel(object):
         
         null_stds = np.array([np.std(d) for d in ds])
         self._distance_norms = null_stds / null_stds[0]
-
+        logging.info('Typical null distance = {}'.format(np.median(ds[0])*\
+                                                        len(self.distance_functions)))
+        return ds
+        
     @property
     def distance_norms(self):
         if not hasattr(self, '_distance_norms'):
@@ -117,3 +159,5 @@ class ABCModel(object):
             ds.append(fn(stats, stats_synth))
 
         return np.sum([d / self.distance_norms[i] for i,d in enumerate(ds)])
+
+
